82ed25987acc1f17c1c9cc847d9ce8898e0839da
Eugene Tsaprilov <gettosun@gmail.com>
2022-05-30 15:12:25 +0300

    refactor: adjust HTML and CSS formatting according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,19 +32,22 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="task-list-item";
     label.innerText=taskString;
-    label.className='task';
+    label.className="task label";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-field";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="btn-image";
     deleteButton.appendChild(deleteButtonImg);
 
 
